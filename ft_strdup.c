792874006c8c4369d21d143a9ca427ab7b526e23
Riyaad DOMINGO <rdomingo@c4r8s8.wethinkcode.co.za>
2019-05-21 09:32:56 +0200

    ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdomingo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 10:22:45 by rdomingo          #+#    #+#             */
+/*   Updated: 2019/05/21 09:32:09 by rdomingo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(char *src)
+{
+	char *s;
+	int len;
+
+	while (src[len])
+		++len
+	if(!(s=(char *)malloc(sizeof(char)*(len + 1))))
+		return(NULL);
+	s[len] = '\0';
+	while (len >= 0)
+	{
+		s[len] = src[len];
+		len--;
+	}
+	return (s);
+}
