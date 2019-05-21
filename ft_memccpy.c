a2aa7f9d8265edac10eb8b3c6fe09a950e0ba298
Riyaad DOMINGO <rdomingo@c4r8s8.wethinkcode.co.za>
2019-05-21 14:40:15 +0200

    ft_memccpy.c

---
+++ ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdomingo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 12:56:14 by rdomingo          #+#    #+#             */
+/*   Updated: 2019/05/21 14:39:37 by rdomingo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void		*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t	i;
+	char	*dst2;
+	char	*src2;
+
+	i = 0;
+	dst2 = (char *)dst;
+	src2 = (char *)src;
+	while (i < n)
+	{
+		dst2[i] = src[i];
+		if ((unsigned char)src2[i] == (unsigned char)c)
+			return ((char *)dst +i +1);
+		i++;
+	}
+	return (NULL);
+}
