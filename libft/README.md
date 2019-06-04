34e29f266ca5be8639799ad12f92aca2b295fe67
cr33de <50695245+cr33de@users.noreply.github.com>
2019-06-04 11:30:47 +0200

    Add files via upload

---
+++ libft/README.md
@@ -0,0 +1,2 @@
+# libft
+it is a recreation of the standard librarys
---
+++ libft/ft_strmap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdomingo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/28 08:45:02 by rdomingo          #+#    #+#             */
+/*   Updated: 2019/05/28 10:26:53 by rdomingo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		str[i] = f(s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
